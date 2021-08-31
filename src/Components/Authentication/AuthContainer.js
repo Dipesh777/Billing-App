ba2538d5129e28dc08baf38824f23b26307284cd
Dipesh <dipesh777rajput@gmail.com>
2021-08-31 15:46:42 +0530

    Client side validation Done for Registration form

--- src/Components/Authentication/AuthContainer.js
+++ src/Components/Authentication/AuthContainer.js
@@ -3,8 +3,12 @@ import Registration from './Registration'
 
 const AuthContainer = (props) => {
     return (
-        <main>
-            <h1>Welcome To Billing App</h1>
+        <main className='m-3 bg-light'>
+            <div className='card bg-success'>
+                <div className='card-body'>
+                    <h1 className='text-white fw-normal text-center'>Get Started With Billing App</h1>
+                </div>
+            </div>
             <Registration />
         </main>
     )
--- src/Components/Authentication/Registration.js
+++ src/Components/Authentication/Registration.js
@@ -10,7 +10,8 @@ const Registration = (props) => {
     const [password, setPassword] = useState('')
     const [email, setEmail] = useState('')
     const [address, setAddress] = useState('')
-
+    const [formError, setFormError] = useState({})
+    const errors = {}
 
 
     // Form Input handler
@@ -29,30 +30,60 @@ const Registration = (props) => {
         }
     }
 
+    // Client side validation errors
+    const validations = () => {
+        if (business.length === 0) {
+            errors.business = 'Business Name is Required'
+        }
+        if (name.length === 0) {
+            errors.name = 'Name is Required'
+        }
+        if (password.length === 0) {
+            errors.password = 'password is Required'
+        }
+        if (email.length === 0) {
+            errors.email = 'email is Required'
+        }
+        if (address.length === 0) {
+            errors.address = 'address is Required'
+        }
+    }
+
     // handling form subbmission
     const handleSubmit = (event) => {
         event.preventDefault()
-        const formData = {
-            username: name,
-            email: email,
-            password: password,
-            businessName: business,
-            address: address
+
+        validations()
+
+        if (Object.keys(errors).length === 0) {
+            setFormError({})
+
+            // formData
+            const formData = {
+                username: name,
+                email: email,
+                password: password,
+                businessName: business,
+                address: address
+            }
+            // redirecting to login
+            const redirect = () => {
+                props.history.push('/login')
+            }
+            // reset form 
+            const resetForm = () => {
+                setBusiness('')
+                setName('')
+                setPassword('')
+                setEmail('')
+                setAddress('')
+            }
+            // dispacting action to register new user
+            dispatch(asyncRegister(formData, redirect, resetForm))
+        } else {
+            setFormError(errors)
         }
-        // redirecting to login
-        const redirect = () => {
-            props.history.push('/login')
-        }
-        // reset form 
-        const resetForm = () => {
-            setBusiness('')
-            setName('')
-            setPassword('')
-            setEmail('')
-            setAddress('')
-        }
-        // dispacting action to register new user
-        dispatch(asyncRegister(formData, redirect, resetForm))
+
     }
 
 
@@ -61,56 +92,66 @@ const Registration = (props) => {
             {/* Registration form */}
             <form style={{ width: '500px' }} className='m-4' onSubmit={handleSubmit}>
 
-                <h2 className='text-success my-4'>Registration</h2>
-                <label>Business Name</label>
+                <h1 className='text-success my-4'>Registration</h1>
+                <label className='fs-4'>Business Name</label>
                 <input
                     type="text"
-                    className='form-control my-2'
+                    className='form-control my-1'
                     name='business'
                     value={business}
                     onChange={handleFormInput}
                 />
-                <label>User Name</label>
+                {formError.business && <span className='text-danger'>{formError.business}</span>} <br />
+
+                <label className='fs-4'>User Name</label>
                 <input
                     type="text"
-                    className='form-control my-2'
+                    className='form-control my-1'
                     name='name'
                     value={name}
                     onChange={handleFormInput}
                 />
-                <label>Password</label>
+                {formError.name && <span className='text-danger'>{formError.name}</span>} <br />
+
+                <label className='fs-4'>Password</label>
                 <input
                     type="password"
-                    className='form-control my-2'
+                    className='form-control my-1'
                     name='password'
                     value={password}
-                    onChange={handleFormInput} />
-                <label>Email</label>
+                    onChange={handleFormInput}
+                />
+                {formError.password && <span className='text-danger'>{formError.password}</span>} <br />
+
+                <label className='fs-4'>Email</label>
                 <input
                     type="email"
-                    className='form-control my-2'
+                    className='form-control my-1'
                     name='email'
                     value={email}
                     onChange={handleFormInput}
                 />
-                <label>Address</label>
+                {formError.email && <span className='text-danger'>{formError.email}</span>} <br />
+
+                <label className='fs-4'>Address</label>
                 <textarea
                     cols="30" rows="3"
-                    className='form-control my-2'
+                    className='form-control my-1'
                     name='address'
                     value={address}
                     onChange={handleFormInput}>
                 </textarea>
+                {formError.address && <span className='text-danger'>{formError.address}</span>} <br />
 
                 {/* submit Buttton */}
                 <input
                     type="submit"
-                    className='btn btn-primary my-2' />
+                    className='btn btn-primary my-1' />
             </form>
 
             {/* Link to Login Page */}
-            <div className='m-4'>
-                <h1 className='text-info'>Already Have an Accout!</h1>
+            <div className='m-5 ps-5 text-center' style={{ paddingTop: '250px' }}>
+                <h1 className=''>Already <br /> Have an Accout!</h1>
                 <p>Login By Click On Login Button</p>
                 <button className='btn btn-primary'><Link to='/' className='text-white text-decoration-none'>Login</Link></button>
             </div>
