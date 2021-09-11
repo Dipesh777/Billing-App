650b2f3c571deb4458d788e25f8017deb988d48b
Dipesh <dipesh777rajput@gmail.com>
2021-09-11 19:25:17 +0530

    class to className inside Dashboard Component

--- src/Components/BillingApp/Dashboard.js
+++ src/Components/BillingApp/Dashboard.js
@@ -20,25 +20,25 @@ const Dashboard = (props) => {
             <section>
                 <div className='d-flex m-4 justify-content-around'>
 
-                    <div class="card text-white bg-success mb-3" style={{ maxWidth: "200px" }}>
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Success card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="card text-white bg-success mb-3" style={{ maxWidth: "200px" }}>
+                        <div className="card-header">Header</div>
+                        <div className="card-body">
+                            <h5 className="card-title">Success card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
-                    <div class="card text-white bg-warning mb-3" style={{ maxWidth: "200px" }}>
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Warning card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="card text-white bg-warning mb-3" style={{ maxWidth: "200px" }}>
+                        <div className="card-header">Header</div>
+                        <div className="card-body">
+                            <h5 className="card-title">Warning card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
-                    <div class="card text-white bg-primary mb-3" style={{ maxWidth: "200px" }}>
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Primary card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div className="card text-white bg-primary mb-3" style={{ maxWidth: "200px" }}>
+                        <div className="card-header">Header</div>
+                        <div className="card-body">
+                            <h5 className="card-title">Primary card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
                 </div>
