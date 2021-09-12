820d40581070947bf84b119b479c7319bab1cbd5
Dipesh <dipesh777rajput@gmail.com>
2021-09-12 17:33:33 +0530

    Dashboard Listed total Customers, Products, and Bills

--- src/Components/BillingApp/Dashboard.js
+++ src/Components/BillingApp/Dashboard.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { startCustomers } from '../../Actions/billingAppActions'
 import { asyncProduct } from '../../Actions/productActions'
 import { startAllBills } from '../../Actions/billingAction'
 
 const Dashboard = (props) => {
     const dispatch = useDispatch()
+    const customers = useSelector((state) => {
+        return state.customers
+    })
+    const products = useSelector((state) => {
+        return state.product
+    })
+    const bills = useSelector((state) => {
+        return state.bills
+    })
+
     useEffect(() => {
         dispatch(startCustomers())
         dispatch(asyncProduct())
         dispatch(startAllBills())
-    })
+    }, [])
     console.log('I am Dashboard')
+
     return (
         <main>
             <header className='m-2 bg-light p-2 text-center text-success'>
@@ -21,24 +32,24 @@ const Dashboard = (props) => {
                 <div className='d-flex m-4 justify-content-around'>
 
                     <div className="card text-white bg-success mb-3" style={{ maxWidth: "200px" }}>
-                        <div className="card-header">Header</div>
+                        <div className="card-header">Customers</div>
                         <div className="card-body">
-                            <h5 className="card-title">Success card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Total Connected Customers</h5>
+                            <p className="card-text text-center fs-1">{customers.length}</p>
                         </div>
                     </div>
                     <div className="card text-white bg-warning mb-3" style={{ maxWidth: "200px" }}>
-                        <div className="card-header">Header</div>
+                        <div className="card-header">Products</div>
                         <div className="card-body">
-                            <h5 className="card-title">Warning card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Total Available Products</h5>
+                            <p className="card-text text-center fs-1">{products.length}</p>
                         </div>
                     </div>
                     <div className="card text-white bg-primary mb-3" style={{ maxWidth: "200px" }}>
-                        <div className="card-header">Header</div>
+                        <div className="card-header">Bills</div>
                         <div className="card-body">
-                            <h5 className="card-title">Primary card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Total Available Bills</h5>
+                            <p className="card-text text-center fs-1">{bills.length}</p>
                         </div>
                     </div>
                 </div>
